a210888fdb179d845a87e8f9ef1fe708abac24ab
Sergei <radchenkovsa@yandex.ru>
2023-08-17 03:27:00 +0300

    Case13

--- case13/Program.cs
+++ case13/Program.cs
@@ -4,3 +4,29 @@
 78 -> третьей цифры нет
 32679 -> 6 */
 
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int thirdNumber(int num)
+{
+    while(num < 999)
+    {
+        num = num / 10;
+    }
+    int thirdNum = num % 10;
+    return thirdNum;
+}
+
+int number = InputNum("Введите число: ");
+if(number < 100)
+{
+    Console.WriteLine("Третья цифра заданного числа отсутствует");
+}
+else
+{
+    int result = thirdNumber(number);
+    Console.WriteLine("Третья цифра заданного числа: {result}");
+}
